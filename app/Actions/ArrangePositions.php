795da61fe492ca9ff0a195a4c4e5066893457ad3
Kaue Pilger <kauesouzapilger@gmail.com>
2024-10-13 11:52:58 +0200

    Altera Query no formato SQLite para MySQL

--- app/Actions/ArrangePositions.php
+++ app/Actions/ArrangePositions.php
@@ -9,14 +9,16 @@ class ArrangePositions
     public static function run(int $id)
     {
         DB::update('
-        with RankedProposals as (
-            select id, row_number() over(order by hours asc) as p
-            from proposals
-            where project_id = :project
-        )
-        update proposals
-        set position = (select p from RankedProposals where proposals.id = RankedProposals.id)
-        where project_id = :project
-    ', ['project' => $id]);
+            UPDATE proposals AS p
+            JOIN (
+                SELECT id, 
+                       ROW_NUMBER() OVER (ORDER BY hours ASC) AS position
+                FROM proposals
+                WHERE project_id = ?
+            ) AS ranked
+            ON p.id = ranked.id
+            SET p.position = ranked.position
+            WHERE p.project_id = ?
+        ', [$id, $id]);
     }
 }
